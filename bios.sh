fbe24720a08fad48c6e8ba7ffc3ce241e2998d5c
dhimanshurao <79211202+dhimanshurao@users.noreply.github.com>
2021-04-03 14:52:27 +0000

    Add files via upload

---
+++ bios.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+#sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+#echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd #replace password with your root password
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+#pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+
+
+pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils nfs-utils inetutils dnsutils cups hplip alsa-utils pulseaudio bash-completion rsync acpi acpi_call bridge-utils dnsmasq ebtables iptables ipset sof-firmware nss-mdns ufw acpid os-prober ntfs-3g
+
+
+
+
+# pacman -S --noconfirm xf86-video-amdgpu
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings # for nvidia drivers
+
+grub-install --target=i386-pc /dev/sdb # replace sdx with your disk name, not the partition
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+#systemctl enable bluetooth
+systemctl enable cups.service
+#systemctl enable sshd
+systemctl enable avahi-daemon
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+#systemctl enable fstrim.timer
+#systemctl enable libvirtd
+systemctl enable ufw
+systemctl enable acpid
+
+useradd -m username  #change username and password accordingly
+echo username:password | chpasswd
+usermod -aG username
+
+echo "username ALL=(ALL) ALL" >> /etc/sudoers.d/username # change username before executing
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+
+
+
